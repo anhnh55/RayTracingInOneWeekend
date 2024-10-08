5139e6df99add151c27af8d7faa2a99aadc37c74
anhnh55 <nha.2731991@gmail.com>
2024-10-09 00:41:59 +0900

    Complete chapter 10.5

--- RaytracingInOneWeekend/RaytracingInOneWeekend/sphere.h
+++ RaytracingInOneWeekend/RaytracingInOneWeekend/sphere.h
@@ -1,14 +1,15 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
+#include "rtweekend.h"
 #include "hittable.h"
-#include "vec3.h"
 
 class sphere : public hittable {
 public:
-    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+    sphere(const point3& center, double radius, shared_ptr<material> mat)
+        : center(center), radius(std::fmax(0, radius)), mat(mat) {}
 
-    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
@@ -21,17 +22,19 @@ public:
         auto sqrtd = std::sqrt(discriminant);
 
         // Find the nearest root that lies in the acceptable range.
-        auto root = (h - sqrtd) / a;
-        if (root <= ray_tmin || ray_tmax <= root) {
-            root = (h + sqrtd) / a;
-            if (root <= ray_tmin || ray_tmax <= root)
+        auto root = (h - sqrtd) / a;//check possible closer hit (smaller solution in quadratic equation)
+        if (!ray_t.surrounds(root)) {
+            root = (h + sqrtd) / a;//check further hit
+            if (!ray_t.surrounds(root)) //doesn't hit anything
                 return false;
         }
 
         //record hit 
         rec.t = root;
         rec.p = r.at(rec.t);
-        rec.normal = (rec.p - center) / radius;
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -39,6 +42,7 @@ public:
 private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
